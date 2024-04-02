d2d5be7cf3ac8bfb67a7241b1122d0c7041d2795
ICE0208 <doky3307@naver.com>
2024-04-02 23:42:12 +0900

    #6.1 Validation Errors

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,15 +1,25 @@
 "use server";
 import { z } from "zod";
 
-const usernameSchema = z.string().min(5).max(10);
+const formSchema = z.object({
+  username: z.string().min(3).max(10),
+  email: z.string().email(),
+  password: z.string().min(10),
+  confirmPassword: z.string().min(10),
+});
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
     username: formData.get("username"),
     email: formData.get("email"),
     password: formData.get("password"),
-    comfirmPassword: formData.get("comfirmPassword"),
+    confirmPassword: formData.get("comfirmPassword"),
   };
 
-  usernameSchema.parse(data.username);
+  const test = formSchema.parse(data);
+  console.log(test);
+  const result = formSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  }
 }
