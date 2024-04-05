1575c02ed17c2148239ad9e1889bb9437d1368ab
ICE0208 <doky3307@naver.com>
2024-04-06 01:57:15 +0900

    #7.4 Relationships

--- lib/db.ts
+++ lib/db.ts
@@ -2,4 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
+async function test() {
+  const token = await db.sMSToken.findUnique({
+    where: {
+      token: "121212",
+    },
+  });
+  console.log(token);
+}
+test();
+
 export default db;
